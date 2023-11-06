2d96d799fcb55e608ae503fefda94103d81cb26c
Esther <eonyando89@gmail.com>
2023-11-06 15:13:44 +0300

    Creating Caesar class for the shift key

--- src/main/java/org/example/Caesar.java
+++ src/main/java/org/example/Caesar.java
@@ -1,18 +1,3 @@
 package org.example;
 
-public class Caesar {
-    private String message;
-    private int shiftKey;
 
-    public Caesar(String message, int shiftKey) {
-        this.message = message.toLowerCase();
-        this.shiftKey = shiftKey;
-    }
-    public String getMessage(){
-        return message;
-    }
-    public int getShiftKey(){
-        return shiftKey;
-    }
-
-}
